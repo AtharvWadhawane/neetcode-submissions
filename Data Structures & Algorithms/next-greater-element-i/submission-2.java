d3f6d251b7fdd7543b0bef1763a3def620889473
Kakarot73 <173579590+AtharvWadhawane@users.noreply.github.com>
2026-08-31 20:25:38 +0530

    Add: next-greater-element-i - submission-2

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        // Precompute next greater for nums2
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            int num = nums2[i];
+            while (!stack.isEmpty() && stack.peek() <= num) {
+                stack.pop();
+            }
+            map.put(num, stack.isEmpty() ? -1 : stack.peek());
+            stack.push(num); //push curr element , potential ans
+        }
+
+        // Build answer for nums1
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+        return ans;
+    }
+}
