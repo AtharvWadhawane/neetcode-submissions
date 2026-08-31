b8822c878cd41cea27f001cd938823eae8ec90d9
Kakarot73 <173579590+AtharvWadhawane@users.noreply.github.com>
2026-08-31 19:30:08 +0530

    Add: next-greater-element-i - submission-1

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[]ans = new int[nums1.length];
+
+        for(int i=0;i<nums1.length;i++){
+            ans[i] = findNext(nums1[i],nums2);
+        }   
+
+        return ans;
+    }
+
+    static int findNext(int num,int[]arr){
+        for(int i=0;i<arr.length;i++){
+            if(num == arr[i]){
+                int j=i+1;
+                while(j<arr.length){
+                    if(arr[j] > num) return arr[j];
+
+                    j++;
+                }
+                return -1; // early exit
+            }
+        }
+        return -1;
+    }
+}
